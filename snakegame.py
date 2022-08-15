6b424dbd5bcf1064f538a0088d237761e6f66a5d
Luis Moncosí Linares <lmoncosi@gmail.com>
2022-08-15 20:23:52 +0200

    Draw a block in the surface

--- snakegame.py
+++ snakegame.py
@@ -5,8 +5,14 @@ from pygame.locals import *
 if __name__== "__main__":
     pygame.init()
 
-    surface= pygame.display.set_mode((500,500))
-    surface.fill((110,110,5))
+    surface= pygame.display.set_mode((1000,500))
+    surface.fill((66,66,0))
+
+    block=pygame.image.load('resources/block.jpg').convert()
+    block_x=100
+    block_y=110
+    surface.blit(block,(block_x,block_y))
+
     pygame.display.flip()
 
 
@@ -15,6 +21,7 @@ if __name__== "__main__":
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                pass
-            elif event.type== QUIT:
+                if event.key == K_ESCAPE:
+                    running=False
+            elif event.type == QUIT:
                 running = False
