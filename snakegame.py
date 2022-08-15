bf66cf765d9f08f854a144d8d8d8ff1f969ed97e
Luis Moncosí Linares <lmoncosi@gmail.com>
2022-08-15 20:04:10 +0200

    Open a Window

--- snakegame.py
+++ snakegame.py
@@ -1,2 +1,20 @@
 import pygame
-import time
+
+from pygame.locals import *
+
+if __name__== "__main__":
+    pygame.init()
+
+    surface= pygame.display.set_mode((500,500))
+    surface.fill((110,110,5))
+    pygame.display.flip()
+
+
+    running= True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                pass
+            elif event.type== QUIT:
+                running = False
