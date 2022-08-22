f5802abd4f8ae528ae6070e9b143c8d94fa7fa93
Luis Moncosí Linares <lmoncosi@gmail.com>
2022-08-22 13:29:55 +0200

    Moving block

--- snakegameOOP.py
+++ snakegameOOP.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import time 
 
 
 class Snake:
@@ -8,28 +9,41 @@ class Snake:
         self.block=pygame.image.load('resources/block.jpg').convert()
         self.x=100
         self.y=110
+        self.direction = 'down'
 
+    def move_up(self):
+        self.direction = 'up'
+
+    def move_down(self):
+        self.direction = 'down'
+
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_right(self):
+        self.direction = 'right'
+    
     def draw(self):
         self.parent_screen.fill((110,110,5))
         self.parent_screen.blit(self.block,(self.x,self.y))
         pygame.display.flip()
 
-    def move_up(self):
-        self.y -=10
-        self.draw()
+    def walk(self):
+        if self.direction == 'up':
+            self.y -=10
 
-    def move_down(self):
-        self.y +=10
-        self.draw()
+        if self.direction == 'down':
+            self.y +=10
 
-    def move_left(self):
-        self.x -=10
-        self.draw()
-
-    def move_right(self):
-        self.x +=10
-        self.draw()
+        if self.direction == 'left':
+            self.x -=10
         
+        if self.direction == 'right':
+            self.x +=10
+
+        self.draw()
+
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -56,8 +70,10 @@ class Game:
                     if event.key== K_RIGHT:
                         self.snake.move_right()
                 elif event.type == QUIT:
-                 running = False 
+                    running = False 
 
+            self.snake.walk()
+            time.sleep(0.1)
 
 if __name__=='__main__':
     game=Game()
